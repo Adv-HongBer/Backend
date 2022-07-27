e07a9a10de558c9aed8bc6b9b371d2378f2c9873
SeoEunGi0802 <lolove08@naver.com>
2022-07-27 23:51:05 +0900

    init

---
+++ src/main/java/com/example/hongber/HongBerApplication.java
@@ -0,0 +1,13 @@
+package com.example.hongber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HongBerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HongBerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hongber/HongBerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hongber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HongBerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
