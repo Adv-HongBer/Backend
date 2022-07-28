662dfa7263c89a34105731351d9109c304d7eb44
SeoEunGi0802 <lolove08@naver.com>
2022-07-29 01:46:19 +0900

    2022-07-29 상수파일 추가

---
+++ src/main/java/com/example/hongber/common/constant/ConstDateFormat.java
@@ -0,0 +1,14 @@
+package com.example.hongber.common.constant;
+
+public class ConstDateFormat {
+    public static final String HHMMSS = "HHmmss";
+    public static final String YYYYMMDD = "yyyyMMdd";
+    public static final String YYYYMMDDHHMMSS = "yyyyMMddHHmmss";
+    public static final String HH_MM_SS = "HH:mm:ss";
+    public static final String YYYY_MM_DD = "yyyy-MM-dd";
+    public static final String YYYY_MM_DD_HH_MM_SS = "yyyy-MM-dd HH:mm:ss";
+    public static final String START_YYYY_MM_DD = "0000-00-00";
+    public static final String LAST_YYYY_MM_DD = "9999-12-31";
+    public static final String START_YYYY_MM_DD_HH_MM_SS = "0000-00-00 00:00:00";
+    public static final String LAST_YYYY_MM_DD_HH_MM_SS = "9999-12-31 23:59:59";
+}
