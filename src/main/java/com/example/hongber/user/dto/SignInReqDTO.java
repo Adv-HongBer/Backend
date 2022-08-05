3b7136abc5759677323e10f3e6c479979cf87dac
SeoEunGi0802 <lolove08@naver.com>
2022-08-05 21:04:35 +0900

    2022-08-05 로그인, 회원가입 관련 DTO생성

---
+++ src/main/java/com/example/hongber/user/dto/SignInReqDTO.java
@@ -0,0 +1,12 @@
+package com.example.hongber.user.dto;
+
+import com.example.hongber.common.dto.BaseDTO;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class SignInReqDTO extends BaseDTO {
+}
