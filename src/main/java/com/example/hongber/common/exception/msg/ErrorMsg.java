b463b5761c428d3eb5a4253cf87f6ccbc9ecc6cd
SeoEunGi0802 <lolove08@naver.com>
2022-08-14 00:16:08 +0900

    2022-08-14 오류 메세지 수정 및 추가

--- src/main/java/com/example/hongber/common/exception/msg/ErrorMsg.java
+++ src/main/java/com/example/hongber/common/exception/msg/ErrorMsg.java
@@ -1,13 +1,15 @@
-package com.example.hongber.common.exception;
+package com.example.hongber.common.exception.msg;
 
 import lombok.Getter;
 
 @Getter
 public enum ErrorMsg {
-    WRONG_PARAMETER("잘못된 파라미터", "파라미터를 확인해주세요."),
+    WRONG_PARAMETER("잘못된 파라미터", "입력값을 확인해주세요."),
     WRONG_TEL_NO("잘못된 형식의 전화번호", "전화번호 형식을 확인해주세요."),
     ENCRYPT_FAIL("암호화 실패", "데이터 암호화 중 오류가 발생하였습니다."),
     DECRYPT_FAIL("복호화 실패", "데이터 복호화 중 오류가 발생하였습니다."),
+    SIGNIN_FAIL("로그인 실패", "로그인 중 문제가 발생하였습니다. 해당 문제가 지속된다면 관리자에게 문의주시길 바랍니다."),
+    SIGNUP_FAIL("회원가입 실패", "회원가입 중 문제가 발생하였습니다. 해당 문제가 지속된다면 관리자에게 문의주시길 바랍니다."),
     ;
 
     private final String logMsg;
