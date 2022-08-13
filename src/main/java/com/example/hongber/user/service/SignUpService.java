daa694b942e537b5a348ea70626ac652f35796d4
SeoEunGi0802 <lolova08@naver.com>
2022-08-14 01:33:01 +0900

    2022-08-14 회원가입 로직 변경

--- src/main/java/com/example/hongber/user/service/SignUpService.java
+++ src/main/java/com/example/hongber/user/service/SignUpService.java
@@ -27,10 +27,11 @@ public class SignUpService {
                 .nickNm(signUpReqDTO.getNickNm())
                 .build();
 
-        try {
-            return signUpRepository.save(userInfo).getIdx();
-        } catch (Exception e) {
+        if (signUpRepository.findAlreadySignUpInfo(userInfo)) {
             throw new BaseException(ErrorMsg.SIGNUP_FAIL.getMsg());
+        } else {
+            return signUpRepository.save(userInfo).getIdx();
+
         }
     }
 }
