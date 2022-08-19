f1d717405f5f23d20d47203c6b8e974abfd99aff
SeoEunGi0802 <lolove08@naver.com>
2022-08-19 23:28:16 +0900

    2022-08-19 코드 정렬

--- src/main/java/com/example/hongber/user/repository/SignInRepository.java
+++ src/main/java/com/example/hongber/user/repository/SignInRepository.java
@@ -5,9 +5,7 @@ import com.example.hongber.user.entity.UserET;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface SignInRepository extends JpaRepository<UserET, Long> {
     // find
     @Encrypt(selOpt = true)
