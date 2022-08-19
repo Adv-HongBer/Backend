8058842c335985b83b201c51df2d52a6cb970b35
SeoEunGi0802 <lolove08@naver.com>
2022-08-19 23:51:39 +0900

    2022-08-19 BaseController 수정 - 로그 추가

--- src/main/java/com/example/hongber/common/controller/BaseController.java
+++ src/main/java/com/example/hongber/common/controller/BaseController.java
@@ -1,19 +1,23 @@
 package com.example.hongber.common.controller;
 
 import com.example.hongber.common.response.RestResponseMessage;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 
+@Slf4j
 public class BaseController {
     // 200 OK
     public <T> ResponseEntity<RestResponseMessage<T>> setOk(T data) {
         RestResponseMessage<T> response = new RestResponseMessage<>();
         response.setData(data);
 
-        return ResponseEntity.status(HttpStatus.OK)
-                .contentType(MediaType.APPLICATION_JSON)
-                .body(response);
+        ResponseEntity<RestResponseMessage<T>> res = ResponseEntity.status(HttpStatus.OK).contentType(MediaType.APPLICATION_JSON).body(response);
+
+        log.info("200 OK, result => {}", res);
+
+        return res;
     }
 
     // 400 Bad Request
@@ -21,9 +25,11 @@ public class BaseController {
         RestResponseMessage<T> response = new RestResponseMessage<>();
         response.setData(data);
 
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                .contentType(MediaType.APPLICATION_JSON)
-                .body(response);
+        ResponseEntity<RestResponseMessage<T>> res = ResponseEntity.status(HttpStatus.BAD_REQUEST).contentType(MediaType.APPLICATION_JSON).body(response);
+
+        log.info("400 Bad Request, result => {}", res);
+
+        return res;
     }
 
     // 403 Forbidden
@@ -31,9 +37,11 @@ public class BaseController {
         RestResponseMessage<T> response = new RestResponseMessage<>();
         response.setData(data);
 
-        return ResponseEntity.status(HttpStatus.FORBIDDEN)
-                .contentType(MediaType.APPLICATION_JSON)
-                .body(response);
+        ResponseEntity<RestResponseMessage<T>> res = ResponseEntity.status(HttpStatus.FORBIDDEN).contentType(MediaType.APPLICATION_JSON).body(response);
+
+        log.info("403 Forbidden, result => {}", res);
+
+        return res;
     }
 
     // 404 Not Found
@@ -41,9 +49,11 @@ public class BaseController {
         RestResponseMessage<T> response = new RestResponseMessage<>();
         response.setData(data);
 
-        return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                .contentType(MediaType.APPLICATION_JSON)
-                .body(response);
+        ResponseEntity<RestResponseMessage<T>> res = ResponseEntity.status(HttpStatus.NOT_FOUND).contentType(MediaType.APPLICATION_JSON).body(response);
+
+        log.info("404 Not Found, result => {}", res);
+
+        return res;
     }
 
     // 500 Internal Server Error
@@ -51,8 +61,10 @@ public class BaseController {
         RestResponseMessage<T> response = new RestResponseMessage<>();
         response.setData(data);
 
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .contentType(MediaType.APPLICATION_JSON)
-                .body(response);
+        ResponseEntity<RestResponseMessage<T>> res = ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).contentType(MediaType.APPLICATION_JSON).body(response);
+
+        log.info("500 Internal Server Error, result => {}", res);
+
+        return res;
     }
 }
